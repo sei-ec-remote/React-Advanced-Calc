45d92ef08f6b9d4588a2ead14988889dd509fd54
chrislee1996 <prepchrislee@gmail.com>
2022-03-25 18:54:21 -0400

    added simple functions to log when something is pressed

--- src/Calculator.js
+++ src/Calculator.js
@@ -2,6 +2,20 @@ import React, { Component } from 'react'
 
 class Calculator extends Component {
     // Declare state variables
+    constructor(props) {
+        super(props)
+        this.state = {
+            total:''
+        }
+    }
+
+    numberInput = (e) => {
+        console.log('number is clicked')
+    }
+
+    signInput = (e) => {
+        console.log('operator is clicked')
+    }
 
 render(){
     return (
@@ -12,31 +26,31 @@ render(){
                 <div className="answer-box">TBD</div>
                 <div className="calc-row">
                     <button className="calc-button calc-button-top">AC</button>
-                    <button className="calc-button calc-button-top">+/-</button>
-                    <button className="calc-button calc-button-top">%</button>
-                    <button className="calc-button calc-button-op">/</button>
+                    <button onClick={this.signInput} className="calc-button calc-button-top">+/-</button>
+                    <button onClick={this.signInput} className="calc-button calc-button-top">%</button>
+                    <button onClick={this.signInput} className="calc-button calc-button-op">/</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">7</button>
-                    <button className="calc-button">8</button>
-                    <button className="calc-button">9</button>
-                    <button className="calc-button calc-button-op">x</button>
+                    <button onClick={this.numberInput} className="calc-button">7</button>
+                    <button onClick={this.numberInput} className="calc-button">8</button>
+                    <button onClick={this.numberInput} className="calc-button">9</button>
+                    <button onClick={this.signInput} className="calc-button calc-button-op">x</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">4</button>
-                    <button className="calc-button">5</button>
-                    <button className="calc-button">6</button>
-                    <button className="calc-button calc-button-op">-</button>
+                    <button onClick={this.numberInput} className="calc-button">4</button>
+                    <button onClick={this.numberInput} className="calc-button">5</button>
+                    <button onClick={this.numberInput} className="calc-button">6</button>
+                    <button onClick={this.signInput} className="calc-button calc-button-op">-</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">1</button>
-                    <button className="calc-button">2</button>
-                    <button className="calc-button">3</button>
-                    <button className="calc-button calc-button-op">+</button>
+                    <button onClick={this.numberInput} className="calc-button">1</button>
+                    <button onClick={this.numberInput} className="calc-button">2</button>
+                    <button onClick={this.numberInput} className="calc-button">3</button>
+                    <button onClick={this.signInput} className="calc-button calc-button-op">+</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button width-2">0</button>
-                    <button className="calc-button">.</button>
+                    <button onClick={this.numberInput} className="calc-button width-2">0</button>
+                    <button onClick={this.numberInput} className="calc-button">.</button>
                     <button className="calc-button calc-button-op">=</button>
                 </div>
             </div>
