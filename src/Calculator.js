331e925f3851f9d7a6e88ff12f1a45775aebcad2
Justin Waddle <justinfrankwaddle@gmail.com>
2022-03-26 17:52:55 -0500

    evaluates the values entered into the calculator

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react'
 
-class Calculator extends Component {
+export default class Calculator extends Component {
     // Declare state variables
     constructor(props) {
         super(props)
         this.state = {
-            numValueOne: "",
-            numValueTwo: "",
+            numValueOne: null,
+            numValueTwo: null,
             operator: "",
-            evalNum: ""
+            evalNum: "",
+            evalBool: false
   
         }
     }
@@ -42,76 +43,87 @@ class Calculator extends Component {
                 //prevents user from entering 0 as first number
                 if (+prevState.numValueOne===0&&+selectedNum===0) return
                 else {
-                    //updates the display with the selectedNum
-                    display.textContent=this.state.numValueOne+selectedNum
-                    return {
-                        //concats number selected to previous number
-                        numValueOne: prevState.numValueOne+selectedNum
+                    if (prevState.numValueOne===null) {
+                        //updates the display with the selectedNum
+                        display.textContent=selectedNum
+                        return {numValueOne: selectedNum}
+                    } else {
+                        //updates the display with the selectedNum
+                        display.textContent=this.state.numValueOne+selectedNum
+                        return {
+                            //concats number selected to previous number
+                            numValueOne: prevState.numValueOne+selectedNum
+                        }
                     }
                 }
             }
             //switches to numValueTwo to be updated
             else {
-                //prevents user from entering 0 as first number
+                //prevents user from entering 0 twice
+                if (prevState.evalBool) return 
                 if (+prevState.numValueTwo===0&&+selectedNum===0) return
                 else {
-                    //updates the display with the selectedNum
-                    display.textContent=this.state.numValueTwo+selectedNum
-                    return {
-                        //concats number selected to previous number
-                        numValueTwo: prevState.numValueTwo+selectedNum
+                    if (prevState.numValueTwo===null) {
+                        //updates the display with the selectedNum
+                        display.textContent=selectedNum
+                        return {numValueTwo: selectedNum}
+                    } else {
+                        //updates the display with the selectedNum
+                        display.textContent=this.state.numValueTwo+selectedNum
+                        return {
+                            //concats number selected to previous number
+                            numValueOne: prevState.numValueTwo+selectedNum
+                        }
                     }
                 }
-
             }
 
         })
     }
 
-    //FUNCTION - evaluates the num1 & num2 with the operator selected
+    //FUNCTION - evaluates the num1 & num2 with the operator selected & updates display
     eval = (e) => {
+        if (this.state.numValueOne===""||this.state.numValueTwo===""||this.state.operator==="") return 
         //assigns display div to variable
         const display = document.getElementById('display')
-
         this.setState((prevState,props) => {
-            if (prevState.numValueOne===""||prevState.numValueTwo===""||prevState.operator==="") return 
-            else {
-                if (prevState.operator==="x") {
-                    //updates the display with the evalNum
-                    display.textContent=+this.state.numValueOne/(+this.state.numValueTwo)
-                    return {
-                        evalNum: parseInt(prevState.numValueOne)/parseInt(prevState.numValueTwo)
-                    }
+            if (prevState.operator==="x") {
+                //updates the display with the evalNum
+                display.textContent=+this.state.numValueOne*(+this.state.numValueTwo)
+                return {
+                    evalNum: parseInt(prevState.numValueOne)*parseInt(prevState.numValueTwo),
+                    evalBool: true
+                } 
 
-                }
-                else if (prevState.operator==="/") {
-                    //updates the display with the evalNum
-                    display.textContent=+this.state.numValueOne/(+this.state.numValueTwo)
-                    return {
-                        evalNum: parseInt(prevState.numValueOne)/parseInt(prevState.numValueTwo)
-                    }
-
-                }
-                else if (prevState.operator==="+") {
-                    //updates the display with the evalNum
-                    display.textContent=+this.state.numValueOne+(+this.state.numValueTwo)
-                    return {
-                        evalNum: parseInt(prevState.numValueOne)+parseInt(prevState.numValueTwo)
-                    }
-
-                }
-                else {
-                    //updates the display with the evalNum
-                    display.textContent=+this.state.numValueOne-(+this.state.numValueTwo)
-                    return {
-                        evalNum: parseInt(prevState.numValueOne)-parseInt(prevState.numValueTwo)
-                    }
-
+            }
+            else if (prevState.operator==="/") {
+                //updates the display with the evalNum
+                display.textContent=+this.state.numValueOne/(+this.state.numValueTwo)
+                return {
+                    evalNum: parseInt(prevState.numValueOne)/parseInt(prevState.numValueTwo),
+                    evalBool: true
                 }
 
             }
+            else if (prevState.operator==="+") {
+                //updates the display with the evalNum
+                display.textContent=+this.state.numValueOne+(+this.state.numValueTwo)
+                return {
+                    evalNum: parseInt(prevState.numValueOne)+parseInt(prevState.numValueTwo),
+                    evalBool: true
+                }
 
+            }
+            else {
+                //updates the display with the evalNum
+                display.textContent=+this.state.numValueOne-(+this.state.numValueTwo)
+                return {
+                    evalNum: parseInt(prevState.numValueOne)-parseInt(prevState.numValueTwo),
+                    evalBool: true
+                }
+            }
         })
+        display.textContent=this.state.evalNum
     }
 
 render(){
@@ -158,4 +170,3 @@ render(){
 }
 }
 
-export default Calculator
