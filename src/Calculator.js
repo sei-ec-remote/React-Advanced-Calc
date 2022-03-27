ed249c585e84f716493e1a0302de82af0733aaf7
“aquador” <Allison.quador@gmail.com>
2022-03-27 16:29:11 -0600

    able to log operators

--- src/Calculator.js
+++ src/Calculator.js
@@ -20,6 +20,10 @@ class Calculator extends Component {
         console.log('this is the clicked value:', e.target.name)
         this.setState({ [name]: parseInt(e.target.name)});
       }
+    setOperator = (e, name) => {
+        console.log('this is the clicked operator:', e.target.name)
+        this.setState({ [name]: e.target.name});
+      }  
       
     //Each button will need an onClick to the e.target.value
     //need this.props => pass data from parent component to child component
@@ -31,34 +35,34 @@ render(){
                 <p>Values: </p>
                 <div className="answer-box">TBD</div>
                 <div className="calc-row">
-                    <button name="AC" className="calc-button calc-button-top"  >AC</button>
-                    <button name="+-" className="calc-button calc-button-top" >+/-</button>
-                    <button name="%" className="calc-button calc-button-top" >%</button>
-                    <button name="/" className="calc-button calc-button-op" >/</button>
+                    <button name="AC" className="calc-button calc-button-top" onClick={ (e) => this.setOperator(e, 'AC') } >AC</button>
+                    <button name="+-" className="calc-button calc-button-top" onClick={ (e) => this.setOperator(e, '+-') }>+/-</button>
+                    <button name="%" className="calc-button calc-button-top" onClick={ (e) => this.setOperator(e, '%') } >%</button>
+                    <button name="/" className="calc-button calc-button-op"onClick={ (e) => this.setOperator(e, '/') } >/</button>
                 </div>
                 <div className="calc-row">
                     <button name="7" className="calc-button" onClick={ (e) => this.setNum(e, '7') } >7</button>
                     <button name="8" className="calc-button" onClick={ (e) => this.setNum(e, '8') } >8</button>
                     <button name="9" className="calc-button" onClick={ (e) => this.setNum(e, '9') } >9</button>
-                    <button name="x" className="calc-button calc-button-op" >x</button>
+                    <button name="x" className="calc-button calc-button-op" onClick={ (e) => this.setOperator(e, 'x') }>x</button>
                 </div>
                 <div className="calc-row">
                     <button name="4" className="calc-button" onClick={ (e) => this.setNum(e, '4') }>4</button>
                     <button name="5" className="calc-button" onClick={ (e) => this.setNum(e, '5') } >5</button>
                     <button name="6" className="calc-button" onClick={ (e) => this.setNum(e, '6') } >6</button>
-                    <button name="-" className="calc-button calc-button-op">-</button>
+                    <button name="-" className="calc-button calc-button-op" onClick={ (e) => this.setOperator(e, '-') }>-</button>
                 </div>
                 <div className="calc-row">
                     <button name="1" className="calc-button" onClick={ (e) => this.setNum(e, '1') }>1</button>
                     <button name="2" className="calc-button" onClick={ (e) => this.setNum(e, '2') }>2</button>
                     <button name="3" className="calc-button" onClick={ (e) => this.setNum(e, '3') }>3</button>
-                    <button name="+" className="calc-button calc-button-op" >+</button>
+                    <button name="+" className="calc-button calc-button-op" onClick={ (e) => this.setOperator(e, '+') }>+</button>
                     
                 </div>
                 <div className="calc-row">
                     <button name="0" className="calc-button width-2" onClick={ (e) => this.setNum(e, '0') } >0</button>
-                    <button name="." className="calc-button" >.</button>
-                    <button name="=" className="calc-button calc-button-op">=</button>
+                    <button name="." className="calc-button" onClick={ (e) => this.setOperator(e, '.') } >.</button>
+                    <button name="=" className="calc-button calc-button-op" onClick={ (e) => this.setOperator(e, '=') }>=</button>
                 </div>
             </div>
         </div>
