aa0b0e14f847cd57d7bf973b567ee9af30f7b22c
dnierman0920 <davidnierman93@gmail.com>
2022-03-27 10:59:06 -0700

    Add state for value and equation && add function addChar to build equation && add components for CurrentValue and EquationBox

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,43 +1,61 @@
 import React, { Component } from 'react'
+import EquationBox from './EquationBox'
+import CurrentValue from './CurrentValue'
 
 class Calculator extends Component {
     // Declare state variables
+    constructor(){
+        super()
+
+        this.state = {
+            value : 'value',
+            equation: ''
+        }
+        
+    }
+
+    addChar = (char) =>{
+        console.log('ADDCHAR IS BEING CALLED, GOOD START!')
+        this.setState(prevState =>{
+            return {equation : prevState.equation+char}
+        })
+    }
 
 render(){
     return (
         <div className="container">
             <h1>React Calculator</h1>
             <div className="calc-container">
-                <p>Values: </p>
-                <div className="answer-box">TBD</div>
+                <CurrentValue value = {this.state.value}/>
+                <EquationBox equation = {this.state.equation}/>
                 <div className="calc-row">
-                    <button className="calc-button calc-button-top">AC</button>
-                    <button className="calc-button calc-button-top">+/-</button>
-                    <button className="calc-button calc-button-top">%</button>
-                    <button className="calc-button calc-button-op">/</button>
+                    <button className="calc-button calc-button-top" >AC</button>
+                    <button className="calc-button calc-button-top" >+/-</button>
+                    <button className="calc-button calc-button-top" onClick={()=>this.addChar('%')}>%</button>
+                    <button className="calc-button calc-button-op" onClick={()=>this.addChar('/')}>/</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">7</button>
-                    <button className="calc-button">8</button>
-                    <button className="calc-button">9</button>
-                    <button className="calc-button calc-button-op">x</button>
+                    <button className="calc-button" onClick={()=>this.addChar(7)}>7</button>
+                    <button className="calc-button" onClick={()=>this.addChar(8)}>8</button>
+                    <button className="calc-button" onClick={()=>this.addChar(9)}>9</button>
+                    <button className="calc-button calc-button-op" onClick={()=>this.addChar('x')}>x</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">4</button>
-                    <button className="calc-button">5</button>
-                    <button className="calc-button">6</button>
-                    <button className="calc-button calc-button-op">-</button>
+                    <button className="calc-button" onClick={()=>this.addChar(4)}>4</button>
+                    <button className="calc-button" onClick={()=>this.addChar(5)}>5</button>
+                    <button className="calc-button" onClick={()=>this.addChar(6)}>6</button>
+                    <button className="calc-button calc-button-op" onClick={()=>this.addChar('-')}>-</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">1</button>
-                    <button className="calc-button">2</button>
-                    <button className="calc-button">3</button>
-                    <button className="calc-button calc-button-op">+</button>
+                    <button className="calc-button" onClick={()=>this.addChar(1)}>1</button>
+                    <button className="calc-button" onClick={()=>this.addChar(2)}>2</button>
+                    <button className="calc-button" onClick={()=>this.addChar(3)}>3</button>
+                    <button className="calc-button calc-button-op" onClick={()=>this.addChar('+')}>+</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button width-2">0</button>
-                    <button className="calc-button">.</button>
-                    <button className="calc-button calc-button-op">=</button>
+                    <button className="calc-button width-2" onClick={()=>this.addChar(0)}>0</button>
+                    <button className="calc-button" onClick={()=>this.addChar('.')}>.</button>
+                    <button className="calc-button calc-button-op" >=</button>
                 </div>
             </div>
         </div>
