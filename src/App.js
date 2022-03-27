6b4eacc6b981fd108ad931725cdf2703d052ef7a
“aquador” <Allison.quador@gmail.com>
2022-03-27 16:22:08 -0600

    able to console.log values clicked

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React, {Component} from 'react';
 import './App.css';
 import Calculator from './Calculator'
 
+
+
 class App extends Component {
   render(){
     return (
       <div className="App">
         <Calculator />
+        
       </div>
     )
   }
--- src/Calculator.js
+++ src/Calculator.js
@@ -2,7 +2,27 @@ import React, { Component } from 'react'
 
 class Calculator extends Component {
     // Declare state variables
-
+    constructor () {
+        super ()
+            this.state = {
+                buttonValues: [
+                    ["AC", "+-", "%", "/"],
+                    ["7", "8", "9", "x"],
+                    ["4", "5", "6", "-"],
+                    ["1", "2", "3", "+"],
+                    ["0", ".", "="]
+                ]
+        }
+        //log the state of the button values array
+        console.log('this is the state', this.state)
+    }
+    setNum = (e, name) => {
+        console.log('this is the clicked value:', e.target.name)
+        this.setState({ [name]: parseInt(e.target.name)});
+      }
+      
+    //Each button will need an onClick to the e.target.value
+    //need this.props => pass data from parent component to child component
 render(){
     return (
         <div className="container">
@@ -11,33 +31,34 @@ render(){
                 <p>Values: </p>
                 <div className="answer-box">TBD</div>
                 <div className="calc-row">
-                    <button className="calc-button calc-button-top">AC</button>
-                    <button className="calc-button calc-button-top">+/-</button>
-                    <button className="calc-button calc-button-top">%</button>
-                    <button className="calc-button calc-button-op">/</button>
+                    <button name="AC" className="calc-button calc-button-top"  >AC</button>
+                    <button name="+-" className="calc-button calc-button-top" >+/-</button>
+                    <button name="%" className="calc-button calc-button-top" >%</button>
+                    <button name="/" className="calc-button calc-button-op" >/</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">7</button>
-                    <button className="calc-button">8</button>
-                    <button className="calc-button">9</button>
-                    <button className="calc-button calc-button-op">x</button>
+                    <button name="7" className="calc-button" onClick={ (e) => this.setNum(e, '7') } >7</button>
+                    <button name="8" className="calc-button" onClick={ (e) => this.setNum(e, '8') } >8</button>
+                    <button name="9" className="calc-button" onClick={ (e) => this.setNum(e, '9') } >9</button>
+                    <button name="x" className="calc-button calc-button-op" >x</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">4</button>
-                    <button className="calc-button">5</button>
-                    <button className="calc-button">6</button>
-                    <button className="calc-button calc-button-op">-</button>
+                    <button name="4" className="calc-button" onClick={ (e) => this.setNum(e, '4') }>4</button>
+                    <button name="5" className="calc-button" onClick={ (e) => this.setNum(e, '5') } >5</button>
+                    <button name="6" className="calc-button" onClick={ (e) => this.setNum(e, '6') } >6</button>
+                    <button name="-" className="calc-button calc-button-op">-</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">1</button>
-                    <button className="calc-button">2</button>
-                    <button className="calc-button">3</button>
-                    <button className="calc-button calc-button-op">+</button>
+                    <button name="1" className="calc-button" onClick={ (e) => this.setNum(e, '1') }>1</button>
+                    <button name="2" className="calc-button" onClick={ (e) => this.setNum(e, '2') }>2</button>
+                    <button name="3" className="calc-button" onClick={ (e) => this.setNum(e, '3') }>3</button>
+                    <button name="+" className="calc-button calc-button-op" >+</button>
+                    
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button width-2">0</button>
-                    <button className="calc-button">.</button>
-                    <button className="calc-button calc-button-op">=</button>
+                    <button name="0" className="calc-button width-2" onClick={ (e) => this.setNum(e, '0') } >0</button>
+                    <button name="." className="calc-button" >.</button>
+                    <button name="=" className="calc-button calc-button-op">=</button>
                 </div>
             </div>
         </div>
