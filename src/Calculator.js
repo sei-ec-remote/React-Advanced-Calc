6a4bd188312b1f34e2043841f4c8f6da92e3177a
jscodeguy <im.still.at.school99@gmail.com>
2022-03-27 19:36:35 -0400

    got numbers to display and have clear display function working

--- src/Calculator.js
+++ src/Calculator.js
@@ -2,6 +2,26 @@ import React, { Component } from 'react'
 
 class Calculator extends Component {
     // Declare state variables
+    constructor() {
+            super()
+            this.state = {
+               display: []
+
+            }
+        }
+    handleClick = (e) => {
+        let buttonNum = e.target.innerText
+            this.setState(() => {
+                return{
+                    display: [...this.state.display, buttonNum]
+                }
+            })
+        }
+clearDisplay = (e) => {
+    this.setState({
+        display: []
+    })
+}
 
 render(){
     return (
@@ -9,33 +29,33 @@ render(){
             <h1>React Calculator</h1>
             <div className="calc-container">
                 <p>Values: </p>
-                <div className="answer-box">TBD</div>
+                <div className="answer-box">{this.state.display}</div>
                 <div className="calc-row">
-                    <button className="calc-button calc-button-top">AC</button>
+                    <button onClick={this.clearDisplay}className="calc-button calc-button-top">AC</button>
                     <button className="calc-button calc-button-top">+/-</button>
                     <button className="calc-button calc-button-top">%</button>
                     <button className="calc-button calc-button-op">/</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">7</button>
-                    <button className="calc-button">8</button>
-                    <button className="calc-button">9</button>
+                    <button onClick={this.handleClick}className="calc-button">7</button>
+                    <button onClick={this.handleClick}className="calc-button">8</button>
+                    <button onClick={this.handleClick}className="calc-button">9</button>
                     <button className="calc-button calc-button-op">x</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">4</button>
-                    <button className="calc-button">5</button>
-                    <button className="calc-button">6</button>
+                    <button onClick={this.handleClick}className="calc-button">4</button>
+                    <button onClick={this.handleClick}className="calc-button">5</button>
+                    <button onClick={this.handleClick}className="calc-button">6</button>
                     <button className="calc-button calc-button-op">-</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">1</button>
-                    <button className="calc-button">2</button>
-                    <button className="calc-button">3</button>
+                    <button onClick={this.handleClick}className="calc-button">1</button>
+                    <button onClick={this.handleClick}className="calc-button">2</button>
+                    <button onClick={this.handleClick}className="calc-button">3</button>
                     <button className="calc-button calc-button-op">+</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button width-2">0</button>
+                    <button onClick={this.handleClick}className="calc-button width-2">0</button>
                     <button className="calc-button">.</button>
                     <button className="calc-button calc-button-op">=</button>
                 </div>
