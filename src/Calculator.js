053b8091792c8b5b0e3361ebeea4ec85d751dde2
stephanieonyekaba <onyek009@umn.edu>
2022-03-28 22:09:49 -0400

    fancy calculator with clickable buttons

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,15 +1,48 @@
+
 import React, { Component } from 'react'
 
 class Calculator extends Component {
     // Declare state variables
+    constructor(){
+        super()
+        this.state = {
+            num1: null,
+            operator: ' ',
+            num2: null,
+            sum: null 
+        }
+        console.log(this.state)
+
+    }
+
+
+//num1 add function 
+    addNum = (e) =>{
+    let numClick = e.target.innerText
+    console.log(numClick)
+        
+    }
+
+
+//num 2 add funtion 
+
+//clear function 
+
+
+
+
 
 render(){
     return (
         <div className="container">
             <h1>React Calculator</h1>
             <div className="calc-container">
-                <p>Values: </p>
-                <div className="answer-box">TBD</div>
+                <p>
+
+                </p>
+
+
+                <div className="answer-box">{this.state.num1}</div>
                 <div className="calc-row">
                     <button className="calc-button calc-button-top">AC</button>
                     <button className="calc-button calc-button-top">+/-</button>
@@ -17,25 +50,25 @@ render(){
                     <button className="calc-button calc-button-op">/</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">7</button>
-                    <button className="calc-button">8</button>
-                    <button className="calc-button">9</button>
+                    <button className="calc-button" onClick={this.addNum}>7</button>
+                    <button className="calc-button" onClick={this.addNum}>8</button>
+                    <button className="calc-button" onClick={this.addNum}>9</button>
                     <button className="calc-button calc-button-op">x</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">4</button>
-                    <button className="calc-button">5</button>
-                    <button className="calc-button">6</button>
+                    <button className="calc-button" onClick={this.addNum}>4</button>
+                    <button className="calc-button" onClick={this.addNum}>5</button>
+                    <button className="calc-button" onClick={this.addNum}>6</button>
                     <button className="calc-button calc-button-op">-</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">1</button>
-                    <button className="calc-button">2</button>
-                    <button className="calc-button">3</button>
+                    <button className="calc-button" onClick={this.addNum}>1</button>
+                    <button className="calc-button" onClick={this.addNum}>2</button>
+                    <button className="calc-button" onClick={this.addNum}>3</button>
                     <button className="calc-button calc-button-op">+</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button width-2">0</button>
+                    <button className="calc-button width-2" onClick={this.addNum}>0</button>
                     <button className="calc-button">.</button>
                     <button className="calc-button calc-button-op">=</button>
                 </div>
---
+++ src/InputBox.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import Calculator from'./Calculator'
+
+export default class IngredientList extends Component {
+    render (){
+
+    }
+    return (
+
+    )
+
+
+}
+
+//this is my input array
+input = []
