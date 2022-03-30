fa54b35f0ee54920ed973936f2c6b345291599f6
stephanieonyekaba <onyek009@umn.edu>
2022-03-29 21:25:11 -0400

    sum operator functioning

--- src/Calculator.js
+++ src/Calculator.js
@@ -7,9 +7,10 @@ class Calculator extends Component {
         super()
         this.state = {
             num1: [],
-            operator: ' ',
+            operator: '',
             num2: [],
-            sum: null 
+            sum: null, 
+            equal: ''
         }
         console.log(this.state)
 
@@ -17,32 +18,67 @@ class Calculator extends Component {
 
 
 //this function is called when each button with a number is clicked 
-    numInput = (e) =>{
-    let numClick = e.target.innerText
-    this.setState({
-        num1: [...this.state.num1, numClick],
+    numInput = (e, num1) =>{
+    let numClick = parseInt(e.target.innerText)
+    console.log("this is num1", this.state.num1)
+    if(this.state.operator === '') {
+        this.setState({
+            num1: [...this.state.num1, numClick],
+        })
+    
+    } else {   
+        this.setState({
         num2: [...this.state.num2, numClick],
-
     })
+    
+    }
         
     }
+
+    
     opInput = (e) =>{
         let opClick = e.target.innerText
         this.setState({
-        operator: opClick
+        operator:opClick
     
         })
             
         }
 
-   
+
+    clearInput = (e) =>{
+        
+            this.setState({
+            num1: [],
+            operator: '',
+            num2: [],
+        
+            })
+                
+            }
+
+    sumInput = (e) => { 
+        let sumClick = parseInt(e.target.innerText)
+        let numVal1 = parseFloat(this.state.num1.join(''))
+        let numVal2 = parseFloat(this.state.num2.join(''))
+        let eq = "="
+            if(this.state.operator == '+') {
+                this.setState({
+                    sum: numVal1 + numVal2,
+                    equa: this.state.eq
             
-
-
-
-
-
-render(){
+                })
+            }
+        }
+        
+        
+        
+        
+        
+        
+        
+    render(){
+    console.log("this is sum input", this.state.sum)
     return (
         <div className="container">
             <h1>React Calculator</h1>
@@ -51,10 +87,12 @@ render(){
 
                 </p>
 
+                
 
-                <div className="answer-box">{this.state.num1} {this.state.operator} {this.state.num2} </div>
+
+                <div className="answer-box">{this.state.num1}{this.state.operator} {this.state.num2} {this.state.eqqals}{this.state.sum}</div>
                 <div className="calc-row">
-                    <button className="calc-button calc-button-top">AC</button>
+                    <button className="calc-button calc-button-top" onClick={this.clearInput}>AC</button>
                     <button className="calc-button calc-button-top">+/-</button>
                     <button className="calc-button calc-button-top">%</button>
                     <button className="calc-button calc-button-op" onClick={this.opInput}>/</button>
@@ -80,7 +118,7 @@ render(){
                 <div className="calc-row">
                     <button className="calc-button width-2" onClick={this.numInput}>0</button>
                     <button className="calc-button">.</button>
-                    <button className="calc-button calc-button-op" onClick={this.addNum}>=</button>
+                    <button className="calc-button calc-button-op" onClick={this.sumInput}>=</button>
                 </div>
             </div>
         </div>
