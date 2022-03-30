350c9a7f3e1f5e52a3da7b1310e213a742af826c
oliviawilcox1 <oliviawilcox07@gmail.com>
2022-03-30 18:08:17 -0400

    Completed Calc

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,48 +1,106 @@
+
 import React, { Component } from 'react'
 
 class Calculator extends Component {
     // Declare state variables
 
-render(){
-    return (
-        <div className="container">
-            <h1>React Calculator</h1>
-            <div className="calc-container">
-                <p>Values: </p>
-                <div className="answer-box">TBD</div>
-                <div className="calc-row">
-                    <button className="calc-button calc-button-top">AC</button>
-                    <button className="calc-button calc-button-top">+/-</button>
-                    <button className="calc-button calc-button-top">%</button>
-                    <button className="calc-button calc-button-op">/</button>
-                </div>
-                <div className="calc-row">
-                    <button className="calc-button">7</button>
-                    <button className="calc-button">8</button>
-                    <button className="calc-button">9</button>
-                    <button className="calc-button calc-button-op">x</button>
-                </div>
-                <div className="calc-row">
-                    <button className="calc-button">4</button>
-                    <button className="calc-button">5</button>
-                    <button className="calc-button">6</button>
-                    <button className="calc-button calc-button-op">-</button>
-                </div>
-                <div className="calc-row">
-                    <button className="calc-button">1</button>
-                    <button className="calc-button">2</button>
-                    <button className="calc-button">3</button>
-                    <button className="calc-button calc-button-op">+</button>
-                </div>
-                <div className="calc-row">
-                    <button className="calc-button width-2">0</button>
-                    <button className="calc-button">.</button>
-                    <button className="calc-button calc-button-op">=</button>
+    constructor() {
+        super()
+        this.state = {
+            num: "",
+            equals: false
+        }
+    }  
+    
+    clickNum = (event) => {
+        let id = event.target.id;
+        console.log(id)
+        // if (this.state.operator === "") {
+        this.setState(prevState => {
+            return {num: prevState.num + id}
+        })
+    //   } else {
+    //       this.setState({
+    //           num2: [...this.state.num2, id]
+    //   })
+    // }
+}
+
+    // operator = (event) => {
+    //     if(event.target.value === 'x') {
+    //         this.setState({
+    //             operator: 'x'
+    //         })
+    //     }
+    // }
+
+    
+    clickEquals = () => {   
+        const string = this.state.num
+        console.log(string)
+        this.setState({
+            num: eval(string),
+            equals: true
+        })
+    }
+    clearButton = () => {
+        
+        this.setState({
+            num: ""
+        })
+    }
+
+// Need four functions to create addition, 
+
+// Addition
+// Subtraction
+// Multiplication
+// Division
+
+
+    render() {
+console.log(this.state.num)
+        return (
+            <div className="container">
+                <h1>React Calculator</h1>
+                <div className="calc-container">
+                    <p>Values: </p>
+                    <div className="answer-box">{this.state.num}</div>
+                    <div className="calc-row">
+                        <button className="calc-button calc-button-top" onClick={this.clearButton}>AC</button>
+                        <button className="calc-button calc-button-top" id="+/-" >+/-</button>
+                        <button className="calc-button calc-button-top" id="%" onClick = {this.clickNum}>%</button>
+                        <button className="calc-button calc-button-op" id="/" onClick = {this.clickNum}>/</button>
+                    </div>
+                    <div className="calc-row">
+                        <button className="calc-button" id="7" onClick = {this.clickNum}>7</button>
+                        <button className="calc-button" id="8" onClick = {this.clickNum}>8</button>
+                        <button className="calc-button" id="9" onClick = {this.clickNum}>9</button>
+                        <button className="calc-button calc-button-op" id="*" onClick = {this.clickNum}>x</button>
+                    </div>
+                    <div className="calc-row">
+                        <button className="calc-button" id="4" onClick = {this.clickNum}>4</button>
+                        <button className="calc-button" id="5" onClick = {this.clickNum}>5</button>
+                        <button className="calc-button" id="6" onClick = {this.clickNum}>6</button>
+                        <button className="calc-button calc-button-op" id="-" onClick = {this.clickNum}>-</button>
+                    </div>
+                    <div className="calc-row">
+                        <button className="calc-button" id="1" onClick = {this.clickNum}>1</button>
+                        <button className="calc-button" id="2" onClick = {this.clickNum}>2</button>
+                        <button className="calc-button" id="3" onClick = {this.clickNum}>3</button>
+                        <button className="calc-button calc-button-op" id="+" onClick = {this.clickNum}>+</button>
+                        
+                    </div>
+                    <div className="calc-row">
+                        <button className="calc-button width-2" onClick = {this.clickNum}>0</button>
+                        <button className="calc-button" id="." onClick = {this.clickNum}>.</button>
+                        <button className="calc-button calc-button-op" id="=" onClick={this.clickEquals}>=</button>
+                    </div>
+                    {console.log('this.state', this.state)}
                 </div>
             </div>
-        </div>
-    )
-}
+        )
+    }
 }
 
 export default Calculator
