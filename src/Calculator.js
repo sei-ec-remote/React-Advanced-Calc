d9402fb3226d6ebd8d86c556dadb5a0467820eaa
LarryMays92 <LarryMays92@ymail.com>
2022-07-26 22:37:57 -0400

    some handles

--- src/Calculator.js
+++ src/Calculator.js
@@ -4,15 +4,60 @@ class Calculator extends Component {
     // Declare state variables
     state = {
         operator: '',
-        Num: '',
+        num1: '',
+        num2: '',
         operation: '',
-        display: '0',
-        sum: ''
+        // display: '0',
+        answer: '0'
     }
-    addNumberToDisplay = (e) => {
+    clearAll = (e) => {
+        this.setState(() => {
+            return {
+                operator: '',
+                num1: '',
+                num2: '',
+                operation: '',
+                answer: ''
+            }
+        })
+    }
+
+    displayNum = (e) => {
         console.log(e.target.innerText)
     }
 
+    handleAdd = () => {
+        const newSum = parseInt(this.state.num1) + parseInt(this.state.num2)
+        this.setState(() => {
+            return {
+                result: newSum,
+            }
+        })
+    }
+    handleSubtract = () => {
+        const newSum = parseInt(this.state.num1) - parseInt(this.state.num2)
+        this.setState(() => {
+            return {
+                result: newSum,
+            }
+        })
+    }
+    handleMultiply = () => {
+        const newSum = parseInt(this.state.num1) * parseInt(this.state.num2)
+        this.setState(() => {
+            return {
+                result: newSum,
+            }
+        })
+    }
+    handleDivide = () => {
+        const newSum = parseInt(this.state.num1) / parseInt(this.state.num2)
+        this.setState(() => {
+            return {
+                result: newSum,
+            }
+        })
+    }
 
 render(){
     return (
@@ -22,32 +67,32 @@ render(){
                 <p>Values: </p>
                 <div className="answer-box">{this.state.display}</div>
                 <div className="calc-row">
-                    <button className="calc-button calc-button-top">AC</button>
+                    <button className="calc-button calc-button-top" onClick={this.clearAll}>AC</button>
                     <button className="calc-button calc-button-top">+/-</button>
                     <button className="calc-button calc-button-top">%</button>
-                    <button className="calc-button calc-button-op">/</button>
+                    <button className="calc-button calc-button-op" onClick={this.handleDivide}>/</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button" onClick={this.addNumberToDisplay}>7</button>
-                    <button className="calc-button" onClick={this.addNumberToDisplay}>8</button>
-                    <button className="calc-button" onClick={this.addNumberToDisplay}>9</button>
-                    <button className="calc-button calc-button-op">x</button>
+                    <button className="calc-button" onClick={this.displayNum}>7</button>
+                    <button className="calc-button" onClick={this.displayNum}>8</button>
+                    <button className="calc-button" onClick={this.displayNum}>9</button>
+                    <button className="calc-button calc-button-op" onClick={this.handleMultiply}>x</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button" onClick={this.addNumberToDisplay}>4</button>
-                    <button className="calc-button" onClick={this.addNumberToDisplay}>5</button>
-                    <button className="calc-button" onClick={this.addNumberToDisplay}>6</button>
-                    <button className="calc-button calc-button-op">-</button>
+                    <button className="calc-button" onClick={this.displayNum}>4</button>
+                    <button className="calc-button" onClick={this.displayNum}>5</button>
+                    <button className="calc-button" onClick={this.displayNum}>6</button>
+                    <button className="calc-button calc-button-op" onClick={this.handleSubtract}>-</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button" onClick={this.addNumberToDisplay}>1</button>
-                    <button className="calc-button" onClick={this.addNumberToDisplay}>2</button>
-                    <button className="calc-button" onClick={this.addNumberToDisplay}>3</button>
-                    <button className="calc-button calc-button-op">+</button>
+                    <button className="calc-button" onClick={this.displayNum}>1</button>
+                    <button className="calc-button" onClick={this.displayNum}>2</button>
+                    <button className="calc-button" onClick={this.displayNum}>3</button>
+                    <button className="calc-button calc-button-op" onClick={this.handleAdd}>+</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button width-2" onClick={this.addNumberToDisplay}>0</button>
-                    <button className="calc-button" onClick={this.addNumberToDisplay}>.</button>
+                    <button className="calc-button width-2" onClick={this.displayNum}>0</button>
+                    <button className="calc-button" onClick={this.displayNum}>.</button>
                     <button className="calc-button calc-button-op">=</button>
                 </div>
             </div>
