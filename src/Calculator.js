513726dac0899d1ce7e5879ee3542d2267fd1811
dangdo85 <danghdo@gmail.com>
2022-07-26 22:36:54 -0400

    operator functions

--- src/Calculator.js
+++ src/Calculator.js
@@ -6,13 +6,62 @@ class Calculator extends Component {
 state = {
     operator: '',
     operation: '',
-    num: '',
-    display: '0',
-    total: ''
+    num1: '',
+    num2: '',
+    total: '0'
+}
+clearAll = (e) => {
+    this.setState(() => {
+        return {
+            operator: '',
+            operation: '',
+            num1: '',
+            num2: '',
+            total: ''
+        }
+    })
 }
 
 displayNum = (e) => {
     console.log(e.target.innerText)
+}
+
+//Operations
+handleAdd = () => {
+    console.log('add')
+    const newSum = parseInt(this.state.num1) + parseInt(this.state.num2)
+    this.setState(() => {
+        return {
+            result: newSum,
+        }
+    })
+}
+handleSubtract = () => {
+    console.log('subtract')
+    const newSum = parseInt(this.state.num1) - parseInt(this.state.num2)
+    this.setState(() => {
+        return {
+            result: newSum,
+        }
+    })
+}
+handleMultiply = () => {
+    console.log('multiply')
+    const newSum = parseInt(this.state.num1) * parseInt(this.state.num2)
+    this.setState(() => {
+        return {
+            result: newSum,
+        }
+    })
+}
+handleDivide = () => {
+    console.log('divide')
+    const newSum = parseInt(this.state.num1) / parseInt(this.state.num2)
+    this.setState(() => {
+        return {
+            result: newSum,
+        }
+    })
 }
 
 render(){
@@ -23,28 +72,28 @@ render(){
                 <p>Values: </p>
                 <div className="answer-box">{this.state.display}</div>
                 <div className="calc-row">
-                    <button className="calc-button calc-button-top">AC</button>
+                    <button className="calc-button calc-button-top" onClick={this.clearAll}>AC</button>
                     <button className="calc-button calc-button-top">+/-</button>
                     <button className="calc-button calc-button-top">%</button>
-                    <button className="calc-button calc-button-op">/</button>
+                    <button className="calc-button calc-button-op" onClick={this.handleDivide}>/</button>
                 </div>
                 <div className="calc-row">
                     <button className="calc-button" onClick={this.displayNum}>7</button>
                     <button className="calc-button" onClick={this.displayNum}>8</button>
                     <button className="calc-button" onClick={this.displayNum}>9</button>
-                    <button className="calc-button calc-button-op">x</button>
+                    <button className="calc-button calc-button-op" onClick={this.handleMultiply}>x</button>
                 </div>
                 <div className="calc-row">
                     <button className="calc-button" onClick={this.displayNum}>4</button>
                     <button className="calc-button" onClick={this.displayNum}>5</button>
                     <button className="calc-button" onClick={this.displayNum}>6</button>
-                    <button className="calc-button calc-button-op">-</button>
+                    <button className="calc-button calc-button-op" onClick={this.handleSubtract}>-</button>
                 </div>
                 <div className="calc-row">
                     <button className="calc-button" onClick={this.displayNum}>1</button>
                     <button className="calc-button" onClick={this.displayNum}>2</button>
                     <button className="calc-button" onClick={this.displayNum}>3</button>
-                    <button className="calc-button calc-button-op">+</button>
+                    <button className="calc-button calc-button-op" onClick={this.handleAdd}>+</button>
                 </div>
                 <div className="calc-row">
                     <button className="calc-button width-2" onClick={this.displayNum}>0</button>
