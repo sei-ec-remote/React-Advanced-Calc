bfd94dd71b5c0aa36a6ebecd94347504ee6d1ade
RandyD <rdsoundguy@gmai.com>
2022-08-01 20:32:47 -0400

    Working with help'

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,48 +1,155 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 
-class Calculator extends Component {
+const Calculator = () => {
+
+    // DON'T USE CONSOLE LOG INSIDE HERE to see State ValUES. IT WILL NOT BE SHOWING CORRECTLY,
+        // UNTIL react evaulates it. Probably not where you think. Maybe use console.log to show where you are
+
     // Declare state variables
 
-render(){
+    // const [inputList, setInputList] = useState([])
+    // const [computeList, setcomputeList] = useState([])
+
+    let answer = ''
+
+    const [lastNumber, setLastNumber] = useState('')
+    const [curentNumber, setcurentNumber] = useState('')
+    const [answerNumber, setAnswerNumber] = useState('')
+
+    const [operator, setOperator] = useState('')
+
+
+    // if button press is number
+    const buttonIsNumber = (clickIndex) => {
+        if(clickIndex){
+            setcurentNumber(curentNumber + clickIndex)
+        }
+
+    }
+    
+    // if button press is non-number
+    const buttonIsNotNumber = (clickIndex) => {
+        // more for error handling
+        if(clickIndex){
+            setOperator(clickIndex)
+            setLastNumber(curentNumber)
+            setcurentNumber('')
+        }
+    }
+
+    const computeIt = (clickIndex) => {
+
+        if(operator === "+"){
+            answer = Number(curentNumber) + Number(lastNumber)
+        }
+        else if (operator === "-"){
+            answer = Number(curentNumber) - Number(lastNumber)
+
+        } else if (operator === "/"){
+            answer = Number(curentNumber) / Number(lastNumber)
+
+        } else if (operator === "*"){
+            answer = Number(curentNumber) * Number(lastNumber)
+
+        } 
+
+        ( setcurentNumber(answer))
+
+    }
+
+
+    const handleClear = (e) => {
+        setLastNumber('')
+        setcurentNumber('')
+        setOperator('')
+    }
+     
+
+    // main flow control if number input or not
+    const handleClick = (e) => {       
+
+        const clickIndex = e.target.value
+        console.log('clickIndex@@',clickIndex);
+
+        let isItTF = Number.isInteger(parseInt(clickIndex))      
+
+        if(isItTF){
+            buttonIsNumber(clickIndex)
+        } else {  
+            buttonIsNotNumber(clickIndex)
+        } 
+
+
+        
+            
+            // if i hit a key
+                // is it a number
+                    // if yes 
+                        // is there an operator
+                            // if yes put compute num1 and num2
+                        // if no operator
+
+            // concat the number
+            // make into int, 
+            // if there an op, then its a 2nd numb, if not then
+            // its a first number
+    }
+            
+
+        // if(clickIndex === "="){
+        //     console.log('=');
+        // } else if (clickIndex === "AC"){
+        //     setInputList([])
+        // } else {
+        //     setInputList(inputList => [...inputList, clickIndex])
+        // }     
+     
+
+
+
+
     return (
+
         <div className="container">
+
+
             <h1>React Calculator</h1>
             <div className="calc-container">
                 <p>Values: </p>
-                <div className="answer-box">TBD</div>
+                <div className="answer-box">{curentNumber}</div>
                 <div className="calc-row">
-                    <button className="calc-button calc-button-top">AC</button>
-                    <button className="calc-button calc-button-top">+/-</button>
-                    <button className="calc-button calc-button-top">%</button>
-                    <button className="calc-button calc-button-op">/</button>
+                    <button className="calc-button calc-button-top" value="AC" onClick={handleClear}>AC</button>
+                    <button className="calc-button calc-button-top" value="±" onClick={handleClick}>+/-</button>
+                    <button className="calc-button calc-button-top" value="%" onClick={handleClick}>%</button>
+                    <button className="calc-button calc-button-op" value="/" onClick={handleClick}>/</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">7</button>
-                    <button className="calc-button">8</button>
-                    <button className="calc-button">9</button>
-                    <button className="calc-button calc-button-op">x</button>
+                    <button className="calc-button" value="7" onClick={handleClick}>7</button>
+                    <button className="calc-button" value="8" onClick={handleClick}>8</button>
+                    <button className="calc-button" value="9" onClick={handleClick}>9</button>
+                    <button className="calc-button calc-button-op" value="*" onClick={handleClick}>x</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">4</button>
-                    <button className="calc-button">5</button>
-                    <button className="calc-button">6</button>
-                    <button className="calc-button calc-button-op">-</button>
+                    <button className="calc-button" value="4" onClick={handleClick}>4</button>
+                    <button className="calc-button" value="5" onClick={handleClick}>5</button>
+                    <button className="calc-button" value="6" onClick={handleClick}>6</button>
+                    <button className="calc-button calc-button-op" value="-" onClick={handleClick}>-</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">1</button>
-                    <button className="calc-button">2</button>
-                    <button className="calc-button">3</button>
-                    <button className="calc-button calc-button-op">+</button>
+                    <button className="calc-button" value="1" onClick={handleClick}>1</button>
+                    <button className="calc-button" value="2" onClick={handleClick}>2</button>
+                    <button className="calc-button" value="3" onClick={handleClick}>3</button>
+                    <button className="calc-button calc-button-op" value="+" onClick={handleClick}>+</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button width-2">0</button>
-                    <button className="calc-button">.</button>
-                    <button className="calc-button calc-button-op">=</button>
+                    <button className="calc-button width-2" value="1" onClick={handleClick}>0</button>
+                    <button className="calc-button" value="." onClick={handleClick}>.</button>
+                    <button className="calc-button calc-button-op" value="=" onClick={computeIt}>=</button>
                 </div>
             </div>
         </div>
     )
-}
+
 }
 
 export default Calculator
