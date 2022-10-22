d5422a56f1f0d2079bc7960ca71c99c5b1ceba82
nabbott98 <nicholasabbott98@gmail.com>
2022-10-21 21:24:43 -0400

    Calculator functional

--- README.md
+++ README.md
@@ -28,25 +28,25 @@ You will need to know the following concepts before attempting this challenge:
 
 You will need to implement the following:
 
-* AC (Clear) button (clear the state)
-* 4 main operators `+ - / *` (store operator in state)
-* 0-9 buttons (store numerical value in state)
-* Equals button (evaluate the answer based on your current state)
+-* AC (Clear) button (clear the state)
+-* 4 main operators `+ - / *` (store operator in state)
+-* 0-9 buttons (store numerical value in state)
+-* Equals button (evaluate the answer based on your current state)
 * Error messages if the user makes a mistake (such as trying to use two operators)
-* A display that shows the current number or current state
+-* A display that shows the current number or current state
    * You can implement it to match the Mac built-in behavior showing the current value only no operator 
    * Alternatively you can show the whole equation if you'd like (as on a graphing calculator)
-* Basic styling to match the Mac built-in app 
+-* Basic styling to match the Mac built-in app 
     * Orange operator buttons
     * Dark gray display
     * Light gray number buttons
     
 **Avoid some potential pitfalls**
 
-* Ensure you can use numbers with multiple digits (e.g., can you add `10 + 20`?)
-   * Make sure you are concatenating each number pressed until you get an operator
-* Don't accept `0` as a first number (e.g., `002` should not be allowed)
-   * Possible exception - if you implement floating point numbers as part of the bonus, you may have a `0` preceeding the decimal point. Still, while `0.1` is valid, `0000.1` is not, so be sure to only add the `0` in front if the `.` is pressed first!
+-* Ensure you can use numbers with multiple digits (e.g., can you add `10 + 20`?)
+   -* Make sure you are concatenating each number pressed until you get an operator
+-* Don't accept `0` as a first number (e.g., `002` should not be allowed)
+  -* Possible exception - if you implement floating point numbers as part of the bonus, you may have a `0` preceeding the decimal point. Still, while `0.1` is valid, `0000.1` is not, so be sure to only add the `0` in front if the `.` is pressed first!
 
 ## Bonus
 
--- src/Calculator.js
+++ src/Calculator.js
@@ -2,42 +2,118 @@ import React, { Component } from 'react'
 
 class Calculator extends Component {
     // Declare state variables
+    state = {
+        num1: '',
+        num2: '',
+        operator: '',
+        currentNum: 1,
+        input: ''
+    }
+
+    allClear = () => {
+        this.setState({
+            num1: '',
+            num2: '',
+            operator: '',
+            currentNum: 1,
+            input: '0'
+        })
+    }
+
+    storeOperator = (e) => {
+        this.setState({
+            operator: e.target.innerText,
+            currentNum: 2,
+            input: ''
+        })
+    }
+
+    storeNum = (e) => {
+        if(this.state.currentNum === 1){
+            if(this.state.num1 === '' && e.target.innerText === '.'){
+                this.setState({
+                    num1: '0.',
+                    input: '0.'
+                })
+            } else if(!(this.state.num1 === '' && e.target.innerText === '0')){
+                this.setState({
+                    num1: this.state.num1.concat(e.target.innerText),
+                    input: this.state.input.concat(e.target.innerText)
+                })
+            }
+        } else {
+            if(this.state.num2 === '' && e.target.innerText === '.'){
+                this.setState({
+                    num2: '0.',
+                    input: '0.'
+                })
+            } else if(!(this.state.num2 === '' && e.target.innerText === '0')){
+                this.setState({
+                    num2: this.state.num2.concat(e.target.innerText),
+                    input: this.state.input.concat(e.target.innerText)
+                })
+            }
+        }
+    }
+
+    math = {
+        '+': function (x, y) { return x + y },
+        '-': function (x, y) { return x - y },
+        'x': function (x, y) { return x * y },
+        '%': function (x, y) { return x % y }
+    }
+
+    equals = () => {
+        if(this.state.operator !== ''){
+            let total = this.math[this.state.operator](+this.state.num1, +this.state.num2)
+            this.setState({
+                num1: `${total}`,
+                num2: '',
+                operator: '',
+                currentNum: 1,
+                input: `${total}`
+            })
+        }
+    }
+
+
 
 render(){
     return (
         <div className="container">
             <h1>React Calculator</h1>
             <div className="calc-container">
-                <p>Values: </p>
-                <div className="answer-box">TBD</div>
+                {/* Not really sure what this is for so I removed it */}
+                {/* <p>Values: </p> */}
+                <div className="answer-box">{this.state.input}</div>
                 <div className="calc-row">
-                    <button className="calc-button calc-button-top">AC</button>
+                    <button className="calc-button calc-button-top" onClick={this.allClear}>AC</button>
                     <button className="calc-button calc-button-top">+/-</button>
-                    <button className="calc-button calc-button-top">%</button>
-                    <button className="calc-button calc-button-op">/</button>
+                    <button className="calc-button calc-button-top" onClick={this.storeOperator}>%</button>
+                    <button className="calc-button calc-button-op" onClick={this.storeOperator}>/</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">7</button>
-                    <button className="calc-button">8</button>
-                    <button className="calc-button">9</button>
-                    <button className="calc-button calc-button-op">x</button>
+                    <button className="calc-button" onClick={this.storeNum}>7</button>
+                    <button className="calc-button" onClick={this.storeNum}>8</button>
+                    <button className="calc-button" onClick={this.storeNum}>9</button>
+                    <button className="calc-button calc-button-op" onClick={this.storeOperator}>x</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">4</button>
-                    <button className="calc-button">5</button>
-                    <button className="calc-button">6</button>
-                    <button className="calc-button calc-button-op">-</button>
+                    <button className="calc-button" onClick={this.storeNum}>4</button>
+                    <button className="calc-button" onClick={this.storeNum}>5</button>
+                    <button className="calc-button" onClick={this.storeNum}>6</button>
+                    <button className="calc-button calc-button-op" onClick={this.storeOperator}>-</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">1</button>
-                    <button className="calc-button">2</button>
-                    <button className="calc-button">3</button>
-                    <button className="calc-button calc-button-op">+</button>
+                    <button className="calc-button" onClick={this.storeNum}>1</button>
+                    <button className="calc-button" onClick={this.storeNum}>2</button>
+                    <button className="calc-button" onClick={this.storeNum}>3</button>
+                    <button className="calc-button calc-button-op" onClick={this.storeOperator}>+</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button width-2">0</button>
-                    <button className="calc-button">.</button>
-                    <button className="calc-button calc-button-op">=</button>
+                    <button className="calc-button width-2" onClick={this.storeNum}>0</button>
+                    <button className="calc-button" onClick={this.storeNum}>.</button>
+                    <button className="calc-button calc-button-op" onClick={this.equals}>=</button>
                 </div>
             </div>
         </div>
