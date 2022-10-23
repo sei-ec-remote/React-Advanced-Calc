feb944e987b7db2b9f61e524da5be6044ef24421
I-AM-ISHIZAKI <ALINAISHIZAKI@OUTLOOK.COM>
2022-10-23 19:02:19 -0400

    trying to create mathematical phrases

--- src/component/Calculator.js
+++ src/component/Calculator.js
@@ -4,21 +4,55 @@ import React, { Component } from 'react'
 class Calculator extends Component {
     // Declare state variables
     state = {
-        '=': '=',
+        result: ' '
     }
 
     // this is for the onClick event for each button
     ////////////////////////////////////////////////
-    // I defined e below, but it didn't have an effect on this event
-    handleInput = (e) => {
-        const value = e.target.value
-        this.setState(() => {
-            return {
-                result: value
-            }
-        })
+    // I defined 'e' below, but it didn't have an effect on this event?
+    // handleInput = (e) => {
+    //     let value = e.target.value
+    //     this.setState(() => {
+    //         return {
+    //             result: value
+    //         }
+    //     })
         
-    }
+    // }
+
+    // psuedo code
+    // handleInput = (e) => {
+    //     let value1 = e.target.value
+    //     let value2 = e.target.value
+    //     let operator = e.target.value
+    //     this.setState(() => {
+    //         return {
+    //             result: (value1, operator, value2)
+    //             // set up if statements for different operators
+    //         }
+    //     })
+        
+    // }
+
+    handleInput = () => {
+        try {
+            this.setState({
+                // eslint-disable-next-line
+                result: (eval(this.state.result) || "" ) + ""
+            })
+        } catch (e) {
+            this.setState({
+                result: "error"
+            })
+
+        } console.log('this is the result', this.state.result)
+    };
+
+    // calculateOperations = () => {
+    //     this.setState({
+    //         result: (eval(this.state.result) || "" ) + ""
+    //     })
+    // }
 
 
 
@@ -64,41 +98,51 @@ class Calculator extends Component {
     //     }
     //   }
 
+        // calculateOperations = () => {
+        //     let value = 'temp math results'
+        //     this.setState(() => {
+        //         return {
+        //             result: value
+        //         }
+        //     })
+
+        // }
+
     render(){
         return (
             <div className="container">
                 <h1>React Calculator</h1>
                 <div className="calc-container">
                     <p>Values: </p>
-                    <div className="answer-box" placeholder="0">{this.state.result}</div>
+                    <div className="answer-box">{this.state.result}</div>
                     <div className="calc-row">
-                        <button className="calc-button calc-button-top"onClick={this.handleInput} name="C" value="clear">AC</button>
-                        <button className="calc-button calc-button-top"onClick={(e) => this.handleInput(e,'7')} name="+/-" value="+/-">+/-</button>
-                        <button className="calc-button calc-button-top"onClick={(e) => this.handleInput(e,'%')} name="%" value="%">%</button>
-                        <button className="calc-button calc-button-op"onClick={(e) => this.handleInput(e,'/')} name="/" value="/">/</button>
+                        <button className="calc-button calc-button-top"onClick={(e) => this.handleInput(e.target.name)} name="C" value=" ">AC</button>
+                        <button className="calc-button calc-button-top"onClick={(e) => this.handleInput(e.target.name)} name="+/-" value="+/-">+/-</button>
+                        <button className="calc-button calc-button-top"onClick={(e) => this.handleInput(e.target.name)} name="%" value="%">%</button>
+                        <button className="calc-button calc-button-op"onClick={(e) => this.handleInput(e.target.name)} name="/" value="/">/</button>
                     </div>
                     <div className="calc-row">
-                        <button className="calc-button"onClick={(e) => this.handleInput(e,'7')} name="7" value="7">7</button>
-                        <button className="calc-button"onClick={(e) => this.handleInput(e,'8')} name="8" value="8">8</button>
-                        <button className="calc-button"onClick={(e) => this.handleInput(e,'9')} name="9" value="9">9</button>
+                        <button className="calc-button"onClick={(e) => this.handleInput(e.target.name)} name="7" value="7">7</button>
+                        <button className="calc-button"onClick={(e) => this.handleInput(e.target.name)} name="8" value="8">8</button>
+                        <button className="calc-button"onClick={(e) => this.handleInput(e.target.name)} name="9" value="9">9</button>
                         <button className="calc-button calc-button-op"onClick={(e) => this.handleInput(e,'')} name="x" value="*">x</button>
                     </div>
                     <div className="calc-row">
-                        <button className="calc-button"onClick={(e) => this.handleInput(e,'4')} name="4" value="4">4</button>
-                        <button className="calc-button"onClick={(e) => this.handleInput(e,'5')} name="5" value="5">5</button>
-                        <button className="calc-button"onClick={(e) => this.handleInput(e,'6')} name="6" value="6">6</button>
+                        <button className="calc-button"onClick={(e) => this.handleInput(e.target.name)} name="4" value="4">4</button>
+                        <button className="calc-button"onClick={(e) => this.handleInput(e.target.name)} name="5" value="5">5</button>
+                        <button className="calc-button"onClick={(e) => this.handleInput(e.target.name)} name="6" value="6">6</button>
                         <button className="calc-button calc-button-op"onClick={(e) => this.handleInput(e,'')} label="-" value="-">-</button>
                     </div>
                     <div className="calc-row">
-                        <button className="calc-button"onClick={(e) => this.handleInput(e,'1')} name="1" value="1">1</button>
-                        <button className="calc-button"onClick={(e) => this.handleInput(e,'2')} name="2" value="2">2</button>
-                        <button className="calc-button"onClick={(e) => this.handleInput(e,'3')} name="3" value="3">3</button>
+                        <button className="calc-button"onClick={(e) => this.handleInput(e.target.name)} name="1" value="1">1</button>
+                        <button className="calc-button"onClick={(e) => this.handleInput(e.target.name)} name="2" value="2">2</button>
+                        <button className="calc-button"onClick={(e) => this.handleInput(e.target.name)} name="3" value="3">3</button>
                         <button className="calc-button calc-button-op"onClick={(e) => this.handleInput(e,'+')} name="+" value="+">+</button>
                     </div>
                     <div className="calc-row">
-                        <button className="calc-button width-2"onClick={(e) => this.handleInput(e,'0')} label="0" value="0">0</button>
-                        <button className="calc-button" onClick={(e) => this.handleInput(e,'.')} label="." value=".">.</button>
-                        <button className="calc-button calc-button-op" onClick={(e) => this.handleInput(e,'=')} label="=" value="equal">=</button>
+                        <button className="calc-button width-2"onClick={(e) => this.handleInput(e.target.name)} label="0" value="0">0</button>
+                        <button className="calc-button" onClick={(e) => this.handleInput(e.target.name)} label="." value=".">.</button>
+                        <button className="calc-button calc-button-op" onClick={(e) => this.handleInput(e.target.name)} label="=" value="equal">=</button>
                     </div>
                 </div>
             </div>
