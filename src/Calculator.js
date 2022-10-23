29571983596a22770f6a1e416980c7ac353f99ee
harrisimon <harrisimon@gmail.com>
2022-10-23 12:30:33 -0400

    completed some bonuses, might come back to percent handling

--- src/Calculator.js
+++ src/Calculator.js
@@ -18,12 +18,13 @@ class Calculator extends Component {
 
         const currButton = e.target.innerText
 
-
-
         if(!(this.state.num1 === '' && currButton === '0') && this.state.operator === ''){
             // handling double decimal
             if (this.state.num1.includes('.') && currButton === '.'){
                 console.log('error!')
+                this.setState(() => {
+                    return {numDisplay: 'Only one decimal per number!'}
+                })
             } else {
                 // setting num1
                 this.setState((prevstate)=>{
@@ -35,11 +36,14 @@ class Calculator extends Component {
             }
         }
         // doesn't allow 0 for first num2 digit
-        if (this.state.num1 !== '' && this.state.operator !== '' && currButton !== '0'){
+        if ((this.state.num1 !== '' && this.state.operator !== '') && currButton !== '0'){
             if(!(this.state.num2 === '' && this.state.operator === '')){
                 //handling double decimal
                 if (this.state.num2.includes('.') && currButton === '.'){
                     console.log('error!')
+                    this.setState(() => {
+                        return {numDisplay: 'Only one decimal per number!'}
+                    })
                 } else {
                     // setting num2
                     this.setState((prevstate) => {
@@ -50,46 +54,85 @@ class Calculator extends Component {
                     })
                 }
             }
-
+        } else if (this.state.num2 !== '' && this.state.operator !== ''){
+            if(!(this.state.num2 === '' && this.state.operator === '')){
+                //handling double decimal
+                if (this.state.num2.includes('.') && currButton === '.'){
+                    console.log('error!')
+                    this.setState(() => {
+                        return {numDisplay: 'Only one decimal per number!'}
+                    })
+                } else {
+                    // setting num2
+                    this.setState((prevstate) => {
+                        return {
+                            num2: prevstate.num2 + currButton,
+                            numDisplay: prevstate.num2 + currButton
+                        }
+                    })
+                }
+            }
         }
-        console.log('num1', this.state.num1)
-        console.log('num2', this.state.num2)
     }
-
+    // set operator and includes if result is filled for chaining operations
     handleOperator = (e) => {
         const operator = e.target.innerText
         if (!(this.state.num1 === '') && this.state.num2 === ''){
             this.setState(() => {
                 return {operator: operator}
             })
+        } else if (!(this.state.result === '')){
+            this.setState(() => {
+                return {operator: operator, num1: this.state.result, num2: ""}
+            })
         }
-        console.log(operator)
     }
 
     handleCalc = () => {
         // if equals is clicked parse integers and complete operation 
+
         if(this.state.result === ''){
             switch (!(this.state.num1 === '' && this.state.num2 === '')) {
                 case this.state.operator === '+':
                     this.setState(() => {
-                        return {result: Number(this.state.num1) + Number(this.state.num2), numDisplay: '' }
+                        return {
+                            result: Number(this.state.num1) + Number(this.state.num2),
+                            numDisplay: '' ,
+                            num1:this.state.result,
+                            num2:this.state.num2
+                        }
                     })
                     break
                 case this.state.operator === '-':
                     this.setState(() => {
-                        return {result: Number(this.state.num1) - Number(this.state.num2), numDisplay: '' }
+                        return {
+                            result: Number(this.state.num1) - Number(this.state.num2),
+                            numDisplay: '',
+                            num1:this.state.result,
+                            num2:this.state.num2 
+                        }
                     })
                     break
                 case this.state.operator === '/':
                     this.setState(() => {
-                        return {result: Number(this.state.num1) / Number(this.state.num2), numDisplay: '' }
+                        return {
+                            result: Number(this.state.num1) / Number(this.state.num2), 
+                            numDisplay: '', 
+                            num1:this.state.result,
+                            num2:this.state.num2 
+                        }
                     })
                     break
                 case this.state.operator === 'x':
                     this.setState(() => {
-                        return {result: Number(this.state.num1) * Number(this.state.num2), numDisplay: '' }
+                        return {
+                            result: Number(this.state.num1) * Number(this.state.num2), 
+                            numDisplay: '', 
+                            num1:this.state.result,
+                            num2:this.state.num2 
+                        }
                     })
-                    break  
+                    break    
                 default:
                     break  
             }
@@ -98,35 +141,76 @@ class Calculator extends Component {
             switch (!(this.state.result ===  '')) {
                 case this.state.operator === '+':
                     this.setState((prevstate) => {
-                        return {result: Number(prevstate.result) + Number(this.state.num2), numDisplay: '' }
+                        return {
+                            result: Number(prevstate.result) + Number(this.state.num2), 
+                            numDisplay: '',
+                            num1:this.state.result,
+                            num2:this.state.num2
+                        }
                     })
                     break
                 case this.state.operator === '-':
                     this.setState((prevstate) => {
-                        return {result: Number(prevstate.result) - Number(this.state.num2), numDisplay: '' }
+                        return {
+                            result: Number(prevstate.result) - Number(this.state.num2), 
+                            numDisplay: '',
+                            num1:this.state.num2,
+                            num2:'' 
+                        }
                     })
                     break
                 case this.state.operator === '/':
                     this.setState((prevstate) => {
-                        return {result: Number(prevstate.result) / Number(this.state.num2), numDisplay: '' }
+                        return {
+                            result: Number(prevstate.result) / Number(this.state.num2), 
+                            numDisplay: '',
+                            num1:this.state.num2,
+                            num2:'' 
+                        }
                     })
                     break
                 case this.state.operator === 'x':
                     this.setState((prevstate) => {
-                        return {result: Number(prevstate.result) * Number(this.state.num2), numDisplay: '' }
+                        return {
+                            result: Number(prevstate.result) * Number(this.state.num2), 
+                            numDisplay: '',
+                            num1:this.state.num2,
+                            num2:''
+                        }
                     })
                     break
+
                 default:
                     break
             }
         }
     } 
+ 
+    // if num1 is not '' and operator is not '' setState for num2 to -
     handleNegative = () => {
-        if(this.state.operator === ''){
+        if(!(this.state.num1 === '') && this.state.operator === ''){
             this.setState((prevstate) => {
                 return { num1: Number(prevstate.num1) * -1, numDisplay:Number(prevstate.num1) * -1 }
             })
+        } else if (!(this.state.num2 === '')) {
+            this.setState((prevstate) => {
+                return { num2: Number(prevstate.num2) * -1, numDisplay:Number(prevstate.num2) * -1 }
+            })
         }
+
+    }
+    
+    handlePercent = () => {
+        // case operator === '%' && this.state.num2 === '':
+        //     this.setState(() => {
+        //         return {result: Number(this.state.num1) / 100 , numDisplay: '' }
+        //     })
+
+        // case operator === '%' && this.state.result !== '':
+        //     this.setState((prevstate) => {
+        //         return {result: Number(prevstate.result) / 100, numDisplay: '' }
+        //     })
+        //     break
     }
     handleClear = () => {
         this.setState(() => {
@@ -150,7 +234,7 @@ render(){
                 <div className="calc-row">
                     <button className="calc-button calc-button-top" onClick={this.handleClear}>AC</button>
                     <button className="calc-button calc-button-top" onClick={this.handleNegative}>+/-</button>
-                    <button className="calc-button calc-button-top" onClick={this.handleOperator}>%</button>
+                    <button className="calc-button calc-button-top" onClick={this.handleCalc}>%</button>
                     <button className="calc-button calc-button-op" onClick={this.handleOperator}>/</button>
                 </div>
                 <div className="calc-row">
