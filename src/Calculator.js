bfc052d87198b8972f176ff06932cee80c975ff0
zyaffee <zyaffee@gmail.com>
2022-10-22 23:11:15 -0400

    basic calculator functionality

--- src/Calculator.js
+++ src/Calculator.js
@@ -5,27 +5,61 @@ class Calculator extends Component {
 
     // Declare state variables
     state = {
-        input: 'asd',
+        inputValue: 'asd',
+        storedValue: 'asd',
         operator: 'asd',
         output: 'asd'
     }
 
     clear = () => {
         this.setState({
-            input: '',
+            inputValue: '',
+            storedValue: '',
             operator: '',
             output: ''
         })
     }
 
     inputChar = (e) => {
-        let newInputState = this.state.input + e.target.innerText
+        let newInputState = this.state.inputValue + e.target.innerText
         this.setState({
-            input: newInputState
+            inputValue: newInputState
         })
     }
 
+    inputOp = (e) => {
+        this.setState({
+            operator: e.target.innerText,
+            storedValue: this.state.inputValue,
+            inputValue: ''
+        })
+    }
 
+    evaluate = () => {
+        let answer = null
+        let x = parseFloat(this.state.inputValue)
+        let y = parseFloat(this.state.storedValue)
+        switch (this.state.operator) {
+            case '+':
+                answer = x + y
+                break              
+            case '-':
+                answer = x - y
+                break
+            case 'x':
+                answer = x * y
+                break
+            case '/':
+                answer = x / y
+                break
+            case '%':
+                answer = x % y
+                break
+        }
+        this.setState({
+            output: answer
+        })
+    }
 
     render() {
 
@@ -42,8 +76,8 @@ class Calculator extends Component {
                         <button className="calc-button calc-button-op">/</button>
                     </div>
                     <div className="calc-row">
-                        <button className="calc-button">7</button>
-                        <button className="calc-button">8</button>
+                        <button onClick={this.inputChar} className="calc-button">7</button>
+                        <button onClick={this.inputChar} className="calc-button">8</button>
                         <button className="calc-button">9</button>
                         <button className="calc-button calc-button-op">x</button>
                     </div>
@@ -57,12 +91,12 @@ class Calculator extends Component {
                         <button className="calc-button">1</button>
                         <button className="calc-button">2</button>
                         <button className="calc-button">3</button>
-                        <button className="calc-button calc-button-op">+</button>
+                        <button onClick={this.inputOp} className="calc-button calc-button-op">+</button>
                     </div>
                     <div className="calc-row">
                         <button className="calc-button width-2">0</button>
                         <button className="calc-button">.</button>
-                        <button className="calc-button calc-button-op">=</button>
+                        <button onClick={this.evaluate} className="calc-button calc-button-op">=</button>
                     </div>
                 </div>
             </div>
