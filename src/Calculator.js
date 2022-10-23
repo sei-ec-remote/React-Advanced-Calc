d2579410c0c08d96a3f5641c1f00c20e5a19de25
OnePartHarmony <bakingharmony@gmail.com>
2022-10-22 20:18:37 -0400

    mostly working -going to try to intall a fix for the floating point math

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,43 +1,174 @@
 import React, { Component } from 'react'
 
 class Calculator extends Component {
-    // Declare state variables
+    
+    state = {
+        operator: "",
+        currentValue: "0",
+        display: "0",
+        value1: ""
+    }
+
+    allClear = () => {
+        this.setState({
+            operator: "",
+            currentValue: "0",
+            display: 0,
+            value1: ""          
+        })
+    }
+
+    evaluate = () => {
+        if (this.state.currentValue !== "") {
+            let num1 = parseFloat(this.state.value1)
+            let num2 = parseFloat(this.state.currentValue)
+            let calculation
+            switch (this.state.operator) {
+                case "+":
+                    calculation = num1 + num2
+                    break
+                case "-":
+                    calculation = num1 - num2
+                    break
+                case "/":
+                    calculation = num1 / num2
+                    break
+                case "*":
+                    calculation = num1 * num2
+                    break
+                default:
+                    return   
+            }
+            this.setState({
+                operator: "",
+                currentValue: "",
+                display: calculation,
+                value1: ""             
+            })            
+        }
+    }
+
+    addToValue = (num) => {
+        if (this.state.currentValue === "0" || this.state.currentValue === "") {
+            this.setState({
+                currentValue: num,
+                display: num
+            })
+        } else {
+            this.setState(prev => {
+                return {
+                    currentValue: prev.currentValue.concat(num),
+                    display: prev.currentValue.concat(num)
+                }
+            }) 
+        }        
+    }
+
+    pressNum = (num) => {
+        if (this.state.currentValue.length <= 10) {
+            this.addToValue(num)
+        }
+        
+    }
+
+    pressZero = () => {
+        if (this.state.currentValue === "0") {
+            return            
+        } else {
+            this.pressNum("0")
+        }
+    }
+
+    percent = () => {
+        if (this.state.currentValue !== "" && this.state.currentValue !== "0") {
+            if (this.state.value1 === "") {
+                this.setState(prev => {
+                    return {
+                        display: prev.display / 100,
+                        value1: prev.display / 100,
+                        currentValue: ""
+                    }                
+                }) 
+            } else {
+                this.setState(prev => {
+                    return {
+                        display: prev.display / 100,
+                        currentValue: prev.display / 100
+                    }                
+                })  
+            }
+        }
+    }
+
+    setOp = (op) => {
+        this.setState(prev => {
+            return {
+                operator: op,
+                value1: prev.display,
+                currentValue: ""
+            }                
+        })        
+    }
+
+    pressOp = (op) => {
+        if (this.state.value1 === "") {
+            this.setOp(op)
+        } else if (this.state.currentValue === "" ) {
+            this.setState({
+                operator: op
+            })
+        } else {
+            this.evaluate()
+            this.setOp(op)
+        }
+    }
+
+    pressPoint = () => {        
+        if (!(this.state.currentValue.includes("."))) {
+            this.setState(prev => {
+                return {
+                    currentValue: prev.currentValue.concat("."),
+                    display: prev.currentValue.concat(".")
+                }
+            }) 
+        }
+    }
 
 render(){
     return (
         <div className="container">
             <h1>React Calculator</h1>
             <div className="calc-container">
-                <p>Values: </p>
-                <div className="answer-box">TBD</div>
+                {/* <p>{this.state.currentValue}</p> */}
+                <div className="answer-box">{this.state.display}</div>
                 <div className="calc-row">
-                    <button className="calc-button calc-button-top">AC</button>
+                    <button className="calc-button calc-button-top" onClick={this.allClear}>AC</button>
                     <button className="calc-button calc-button-top">+/-</button>
-                    <button className="calc-button calc-button-top">%</button>
-                    <button className="calc-button calc-button-op">/</button>
+                    <button className="calc-button calc-button-top" onClick={this.percent}>%</button>
+                    <button className={`calc-button calc-button-op ${this.state.operator === "/" ? "activeOp" : ""}`} onClick={() => {this.pressOp("/")}}>/</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">7</button>
-                    <button className="calc-button">8</button>
-                    <button className="calc-button">9</button>
-                    <button className="calc-button calc-button-op">x</button>
+                    <button className="calc-button" onClick={() => {this.pressNum("7")}}>7</button>
+                    <button className="calc-button" onClick={() => {this.pressNum("8")}}>8</button>
+                    <button className="calc-button" onClick={() => {this.pressNum("9")}}>9</button>
+                    <button className={`calc-button calc-button-op ${this.state.operator === "*" ? "activeOp" : ""}`} onClick={() => {this.pressOp("*")}}>x</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">4</button>
-                    <button className="calc-button">5</button>
-                    <button className="calc-button">6</button>
-                    <button className="calc-button calc-button-op">-</button>
+                    <button className="calc-button" onClick={() => {this.pressNum("4")}}>4</button>
+                    <button className="calc-button" onClick={() => {this.pressNum("5")}}>5</button>
+                    <button className="calc-button" onClick={() => {this.pressNum("6")}}>6</button>
+                    <button className={`calc-button calc-button-op ${this.state.operator === "-" ? "activeOp" : ""}`} onClick={() => {this.pressOp("-")}}>-</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">1</button>
-                    <button className="calc-button">2</button>
-                    <button className="calc-button">3</button>
-                    <button className="calc-button calc-button-op">+</button>
+                    <button className="calc-button" onClick={() => {this.pressNum("1")}}>1</button>
+                    <button className="calc-button" onClick={() => {this.pressNum("2")}}>2</button>
+                    <button className="calc-button" onClick={() => {this.pressNum("3")}}>3</button>
+                    <button className={`calc-button calc-button-op ${this.state.operator === "+" ? "activeOp" : ""}`} onClick={() => {this.pressOp("+")}}>+</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button width-2">0</button>
-                    <button className="calc-button">.</button>
-                    <button className="calc-button calc-button-op">=</button>
+                    <button className="calc-button width-2" onClick={this.pressZero}>0</button>
+                    <button className="calc-button" onClick={this.pressPoint}>.</button>
+                    <button className="calc-button calc-button-op" onClick={this.evaluate}>=</button>
                 </div>
             </div>
         </div>
